388c2782fd707d981fd891386ccbb26ad264f185
JoeyyT <joeyteuissen@msn.com>
2019-12-10 10:13:27 +0100

    fixed the daterangepicker structure

--- projects/uilib/src/public-api.ts
+++ projects/uilib/src/public-api.ts
@@ -6,3 +6,7 @@
 export * from './lib/modules/boilerplate.module'
 export * from './lib/components/boilerplate/boilerplate.component'
 export * from './lib/services/boilerplate/boilerplate.service'
+export * from './lib/services/boilerplate/boilerplate.service'
+export * from './lib/daterangepicker.service';
+export * from './lib/daterangepicker.component';
+export * from './lib/daterangepicker.module';
